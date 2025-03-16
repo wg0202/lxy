14eec2c0c90ae12b48ea2b4b7a77dbbcd7b0d648
wg0202 <nicainicai0822@qq.com>
2025-03-16 10:04:54 +0800

    Update Wuziqi.java
    修正了代码编译错误

--- src/Wuziqi.java
+++ src/Wuziqi.java
@@ -1,6 +1,6 @@
 public class Wuziqi {
     public static void main(String[] args) {
-        String name = "李欣宇"
+        String name = "李欣宇";
         System.out.println("hello" + ",李欣宇");
     }
 }
