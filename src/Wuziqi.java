2b71b5831c671958c8a8d482070dd5453094b5f6
nicai <nicainicai0822@qq.com>
2025-03-16 10:20:21 +0800

    这是一个你好 增加了一个可以显示个人信息的方法

--- src/Wuziqi.java
+++ src/Wuziqi.java
@@ -1,6 +1,11 @@
 public class Wuziqi {
     public static void main(String[] args) {
+        int age = 20;
         String name = "李欣宇";
-        System.out.println("hello" + ",李欣宇");
+        System.out.println("hello," + age + "岁的李欣宇");
+        show(20,"李欣宇");
+    }
+    public static void show(int age,String name){
+        System.out.println(age + name);
     }
 }
